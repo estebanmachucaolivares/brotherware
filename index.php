5c8ec874023601e024f918194e895c11d4e6f33a
esteban machuca <esteban.patricio@live.com>
2020-01-07 22:39:11 -0300

    mover documentos a directorio admin

--- index.php
+++ index.php
@@ -1,60 +1,65 @@
-<?php @session_start();
-  if (isset($_SESSION['nick'])) {
-    header('location:inicio');
-  }
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/materialize.min.css">
+    <link rel="stylesheet" href="admin/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  
-    <title>Document</title>
+    <title>BrotherWare</title>
 </head>
-<body class="black">
-    <main >
-        <div class="row">
-            <div class="input-field col s12 center">
-             <img src="img/iconbw.png" alt="" width="200" >
-            </div>
+<body>
+    <nav class="red"> 
+  <div class="nav-wrapper">
+      <a href="#" class="brand-logo"><img src="admin/img/iconbw.png" width="60" height="60" class="responsive-img" ></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+    </nav>
+
+
+  <div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="admin/img/fondo.png" > <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
         </div>
-        <div class="container">
-           <div class="row">
-              <div class="col s12">
-                 <div class="card z-depth-5">
-                   <div class="card-content">
-                   <span class="card-title "><center>INICIO DE SESION</center> </span>
-                   <form action="login/index.php" method="post" autocomplete="off">
-                     <div class="input-field">
-                         <i class="material-icons prefix">perm_identity</i>
-                         <input type="text" name="usuario" id="usiuario" pattern="[A-Za-z]{8,15}" autofocus  required >
-                         <label for="usuario">Usuario</label>
-                     </div>
-                     <div class="input-field">
-                     <i class="material-icons prefix">vpn_key</i>
-                         <input type="password" name="contra" id="contra" pattern="[A-Za-z0-9]{8,15}" required >
-                         <label for="contra">Contraseña</label>
-                     </div>
-                     <div class="input-field center">
-                      <button type="submit" class="btn waves-effect waves-light"> Acceder</button>
-                     </div>
-                   </form>
-                   </div>
-                </div>   
-              </div>
-            </div>   
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/2"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Left Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
         </div>
-    </main>
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/3"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="https://lorempixel.com/580/250/nature/4"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
+
 
 <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
   crossorigin="anonymous"></script>
-
-  <script src="js/materialize.min.js"></script>
+  <script src="admin/js/materialize.min.js"></script>
+  <script> $('.slider').slider(); </script>
 </body>
 </html>
